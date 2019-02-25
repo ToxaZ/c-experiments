009997fe9af0255ca781442326ba7557dcd921c5
az <anton.zayniev@team.wrike.com>
2019-02-25 23:52:31 +0300

    newshound program

---
+++ newshound.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+	char *feeds[] = {
+		"http://rss.cnn.com/rss/edition.rss",
+		"https://news.yandex.ru/music.rss",
+		"https://www.nasa.gov/rss/dyn/breaking_news.rss"
+	};
+	int times = 3;
+	char *phrase = argv[1];
+	int i;
+	for (i = 0; i < times; i++) {
+		char var[255];
+		sprintf(var, "RSS_FEED=%s", feeds[i]);
+		char *vars[] = {var, NULL};
+		pid_t pid = fork();
+		if (pid == -1) {
+			fprintf(stderr, "Can't fork process: %s\n", strerror(errno));
+			return 1;
+		} 
+		if (!pid) {
+			if (execle(
+				"/usr/bin/python",
+				"/usr/bin/python",
+				"rss_gossip/rssgossip.py",
+				phrase, NULL, vars) == -1) {
+					fprintf(stderr, "Can't run script: %s\n", strerror(errno));
+				return 1;
+			}
+		}
+	}
+	return 0;
+}
